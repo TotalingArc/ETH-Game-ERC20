242038718e7edd81e338bee605058019561f4623
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2024-06-08 11:10:08 -0600

    Update GameToken.sol

--- contracts/GameToken.sol
+++ contracts/GameToken.sol
@@ -2,22 +2,19 @@
 
 pragma solidity >=0.4.22 <0.9.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Gametoken is ERC20, ERC20Detailed {
+contract Gametoken is ERC20 {
 
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) 
+        ERC20(_name, _symbol)
+        public 
+    {
+        _setupDecimals(_decimals);
+    }
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals)
-    ERC20Detailed(_name, _symbol, _decimals)
-    public
-    {}
-
-       function mint(address account, uint256 amount) public returns (bool) {
+    function mint(address account, uint256 amount) public returns (bool) {
         _mint(account, amount);
         return true;
     }
-
-
-
 }
